a57f2fbcad3018d3f0c809a219509744fc167d4c
Mateo Lopez-Castillo <mateolopez-castillo@Mateos-MacBook-Air.local>
2022-10-31 12:08:48 -0400

    config

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose"
 
 let mongooseConfig = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useFindAndModify:true,
+    useUnifiedTopology: true
 }
 
 mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/soccer_tables_API", mongooseConfig)
 
-mongoose.connection.on('connected', () => console.log('Connected to database'))
-mongoose.connection.on('disconnected', () => console.log('Disconnected from database'))
-mongoose.connection.on('error', (error) => console.error('Error:', error))
+async mongoose.connection.on('connected', () => console.log('Connected to database'))
+async mongoose.connection.on('disconnected', () => console.log('Disconnected from database'))
+async mongoose.connection.on('error', (error) => console.error('Error:', error))
 
 
 export default mongoose.connection
