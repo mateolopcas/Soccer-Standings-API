33371fd8f757846f18818bbe6a49e1904d71197b
Mateo Lopez-Castillo <mateolopez-castillo@Mateos-MacBook-Air.local>
2022-10-31 11:37:41 -0400

    db

--- seed/seed.js
+++ seed/seed.js
@@ -3,6 +3,7 @@ import League from '../models/League.js'
 import data from './seasons_data.json' assert {type: 'json'}
 
 async function insertData() {
+    await db.runCommand( { dropDatabase: 1 } )
     await League.create(data)
     await db.close()
 }
