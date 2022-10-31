769401ee2ca406df379449e62184155d57ceb7ad
Mateo Lopez-Castillo <mateolopez-castillo@Mateos-MacBook-Air.local>
2022-10-31 12:20:27 -0400

    URI

--- db/connection.js
+++ db/connection.js
@@ -7,11 +7,11 @@ let mongooseConfig = {
     useUnifiedTopology: true
 }
 
-mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/soccer_tables_API", mongooseConfig)
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/soccer_tables_API", mongooseConfig)
 
-async mongoose.connection.on('connected', () => console.log('Connected to database'))
-async mongoose.connection.on('disconnected', () => console.log('Disconnected from database'))
-async mongoose.connection.on('error', (error) => console.error('Error:', error))
+mongoose.connection.on('connected', () => console.log('Connected to database'))
+mongoose.connection.on('disconnected', () => console.log('Disconnected from database'))
+mongoose.connection.on('error', (error) => console.error('Error:', error))
 
 
 export default mongoose.connection
