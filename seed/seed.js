209f1d738ed67fd887f8f73035cfa3a862bf5c12
Mateo Lopez-Castillo <mateolopez-castillo@Mateos-MacBook-Air.local>
2022-10-31 11:07:29 -0400

    add dropdatabase again

--- seed/seed.js
+++ seed/seed.js
@@ -3,6 +3,7 @@ import League from '../models/League.js'
 import data from './seasons_data.json' assert {type: 'json'}
 
 async function insertData() {
+    await db.dropDatabase()
     await League.create(data)
     await db.close()
 }
